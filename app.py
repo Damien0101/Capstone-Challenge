105f8beb5e16a202035fba129fee2b80f91de96f
Damien <damcops1@gmail.com>
2024-11-10 22:51:34 +0100

    update app

--- app.py
+++ app.py
@@ -11,34 +11,37 @@ st.title("Trend tracker 📈")
 st.markdown("##### Easily discover if a word or phrase is trending with this app. Enter a keyword or sentence and see if it’s gaining popularity. Made for journalists looking to choose articles with current trends, the app provides a visual timeline of the trend’s activity helping you identify the best topics to cover.")
 
 with st.sidebar:
-    st.title('title')
+    st.title('⚙️ Settings')
+    st.sidebar.write("**Tip**: Try shorter date ranges to focus on recent changes in trends.")
+
+    start = st.date_input('start date')
+    end = st.date_input('end date')
+
 
 pytrends = TrendReq(hl='fr-FR', tz=360)
 user_input = st.text_input(label='**Enter a word/sentence**')
 
 if user_input:
-    pytrends.build_payload([user_input], cat=0, timeframe='today 3-m', geo='BE')
-    trend_data = pytrends.interest_over_time()
-    
-    three_months_ago = datetime.now() - timedelta(days=90)
-    trend_data = trend_data[trend_data.index > three_months_ago]
+    if start and end:
+        timeframe = f'{start.strftime("%Y-%m-%d")} {end.strftime("%Y-%m-%d")}'
+        pytrends.build_payload([user_input], cat=0, timeframe=timeframe, geo='BE')
+        trend_data = pytrends.interest_over_time()
 
-    dates = trend_data.index.to_list()
-    day_month = [date.strftime('%m-%d') for date in dates]
-    new_date = [f'{date[3:]} - {date[:2]}' for date in day_month]
+        if not trend_data.empty:
+            dates = trend_data.index.to_list()
+            formatted_dates = [date.strftime('%d - %m - %y') for date in dates]
 
+            keyword_trend_list = trend_data[trend_data.columns[0]].tolist()
 
-    keyword_trend_list = trend_data[trend_data.columns[0]].tolist()
-    
-    data_lst = [new_date, keyword_trend_list]
+            data_lst = [formatted_dates, keyword_trend_list]
 
-    df = pd.DataFrame(data_lst).transpose()
-    df.columns = ['Date (day - month)', 'Value (%)']
+            df = pd.DataFrame(data_lst).transpose()
+            df.columns = ['Date', 'Value (%)']
 
-    fig = px.area(df, x="Date (day - month)", y="Value (%)", color="Value (%)", line_group="Value (%)")
-    fig.update_layout(title=f'Trend of "{user_input}" over the last 3 months', xaxis_tickangle=-45, width=1000, height=500)
+            fig = px.area(df, x="Date", y="Value (%)")
+            fig.update_layout(title=f'Trend of "{user_input}" from {start.strftime('%d/%m/%y')} to {end.strftime('%d/%m/%y')}', xaxis_tickangle=-45, width=1000, height=500)
+            fig.update_xaxes(nticks=15)
+            st.plotly_chart(fig)
 
-    st.plotly_chart(fig)
-    
-    max_value_date = df.loc[df['Value (%)'].idxmax(), 'Date (day - month)']
-    st.markdown(f'#### The date where there is the highest value is: {max_value_date} 💡')
+            max_value_date = df.loc[df['Value (%)'].idxmax(), 'Date']
+            st.markdown(f'#### The date where the highest value is: {max_value_date} 💡')
--- model/model.py
+++ model/model.py
@@ -10,6 +10,8 @@ pytrends = TrendReq(hl='fr-FR', tz=360)
 pytrends.build_payload(keywords, cat=0, timeframe='today 12-m', geo='BE')
 trend_data = pytrends.interest_over_time()
 
+print(trend_data)
+
 three_months_ago = datetime.now() - timedelta(days=90)
 trend_data = trend_data[trend_data.index > three_months_ago]
 
@@ -18,14 +20,3 @@ day_month = [date.strftime('%m-%d') for date in dates]
 
 keyword_trend_list = trend_data[trend_data.columns[0]].tolist()
 
-plt.figure(figsize=(10, 5))
-plt.plot(day_month, keyword_trend_list, marker='o', linestyle='-')
-plt.xlabel('date')
-plt.ylabel('trend Value')
-plt.title(f'trend over time for keyword "{keywords[0]}"')
-plt.xticks(rotation=45)
-plt.tight_layout()
-plt.show()
-
-# print(keyword_trend_list)
-# print(day_month)
